0cc48f7453c3ca7892acfa6607c7fc3add575026
harvey103565 <harvey103565@gmail.com>
2018-10-12 17:02:11 +0800

    Optimized knapsack (Zero/One) dp solution.

--- dp/knapsack.py
+++ dp/knapsack.py
@@ -6,15 +6,14 @@ class Knapsack:
         self._volume = volume
 
     def ZeroOneSack(self, v: iter, c: iter) -> list:
-        cnt = len(v)
+        n = len(v)
 
-        dp = np.zeros((cnt, self._volume + 1), dtype=np.int)
-        # dp = [[0] * self._volume] * cnt
+        dp = np.zeros((n, self._volume + 1), dtype=np.int)
         for j in range(self._volume + 1):
             if j >= c[0]:
                 dp[0][j] = v[0]
         
-        for i in range(1, cnt):
+        for i in range(1, n):
             for j in range(self._volume + 1):
                 if j + 1 > c[i]:
                     dp[i][j] = max((dp[i - 1][j - c[i]] + v[i]), dp[i - 1][j])
@@ -24,11 +23,23 @@ class Knapsack:
         return dp
 
     def ZeroOneSack_Opt(self, v: list, c: list) -> list:
-        return None
+        n = len(v)
+
+        dp = np.zeros(self._volume + 1, dtype=np.int)
+
+        for i in range(n):
+            for j in range(self._volume, 0, -1):
+                if j > c[i]:
+                    dp[j] = max(dp[j - c[i]] + v[i], dp[j])
+
+        return dp
     
 
 knapsack = Knapsack(15)
 
-r = knapsack.ZeroOneSack((12, 3, 10, 3, 6), (5, 4, 7, 2, 6))
-print(r)
+r1 = knapsack.ZeroOneSack((12, 3, 10, 3, 6), (5, 4, 7, 2, 6))
+r2 = knapsack.ZeroOneSack_Opt((12, 3, 10, 3, 6), (5, 4, 7, 2, 6))
+print(r1)
+print(r2)
 
+pass
