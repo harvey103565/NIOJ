2f28b4859051db349a918351f6cc686fbed6d014
harvey103565 <harvey103565@gmail.com>
2018-11-05 17:22:55 +0800

    leetcode 15 3-sum, Working

--- leetcode/15-3 sum.py
+++ leetcode/15-3 sum.py
@@ -4,44 +4,70 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        l = sorted(nums)
-        n = len(l)
-        combinations = []
+        sorted_nums = sorted(nums)
+        n = len(sorted_nums)
+        result = []
 
-        for i in (range(2, n)):
-            if l[i] < 0:
-                continue
+        def binary_seek(a: list, k: int, l: int, r: int):
+            p = (l + r) // 2
+            while(l < r):
+                if a[p] > k:
+                    r = p
+                elif a[p] < k:
+                    l = p + 1
+                else:
+                    break
+                p = (l + r) // 2
+            return p
 
-            if sum(l[i - 2: i + 1]) < 0:
-                continue
+        def move_next(a: list, s: int, e: int, d: int) -> int:
+            v = a[s]
+            while a[s] == v and s != e:
+                s = s + d
+            return s
 
-            break
-        else:
-            return combinations
+        l, r = 0, n - 1
+        while l < r - 1:
+            i, k = l, r
+            while i < k - 1:
+                target_sum = -(sorted_nums[i] + sorted_nums[r])
 
-        ti = l[i - 2] - 1
-        for i in range(i - 2, n):
-            if l[i] = ti:
-                continue
+                if sorted_nums[i] <= target_sum <= sorted_nums[r]:
+                    k = binary_seek(sorted_nums, target_sum, i + 1, k - 1)
+                    if sorted_nums[k] > target_sum:
+                        r = move_next(sorted_nums, r, l + 1, -1)
+                        break
+                    else:
+                        if sorted_nums[k] == target_sum:
+                            result.append((sorted_nums[i], sorted_nums[k], sorted_nums[r]))
+                        else:
+                            k += 1
+                        i = move_next(sorted_nums, i, k, 1)
+                else:
+                    if target_sum > sorted_nums[r]:
+                        l = move_next(sorted_nums, l, k, 1)
+                    else:
+                        r = move_next(sorted_nums, r, l + 1, -1)
+                    break
+            else:
+                r = move_next(sorted_nums, r, l + 1, -1)
 
-            tj = l[i + 1] - 1
-            for j in range(i + 1, n):
-                if l[j] == tj:
-                    continue
-
-                tk = l[j + 1] - 1
-                for k in range(j + 1, n):
-                    if l[k] == tk:
-                        continue
-                    
-                    combinations.append((l[i], l[j], [k]))
-
-                tj = l[j]
-            ti = l[i]
-
-        return combinations
-            
+        return result
 
 
-            
 
+s = Solution()
+
+r = s.threeSum([-1, 0, 1, 2, -1, -4])
+print(r)
+
+r = s.threeSum([0, 0, 0, 0, 0, 0])
+print(r)
+
+r = s.threeSum([3,0,-2,-1,1,2])
+print(r)
+
+r = s.threeSum([-4,-1,-4,0,2,-2,-4,-3,2,-3,2,3,3,-4])
+print(r)
+
+pass
